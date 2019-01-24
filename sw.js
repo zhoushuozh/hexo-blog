a1fd0d78873e9b5d20a1378cbf912f94b1f2f3ef
zhoushuo <zhoushuozh@gmail.com>
2019-01-24 17:08:42 +0800

    Site updated: 2019-01-24 17:08:40

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.keys().then(function (names) {
-      var validSets = ["is-sw-58a1db","is-html-58a1db"];
+      var validSets = ["is-sw-954823","is-html-954823"];
       return Promise.all(
         names
           .filter(function (name) { return !~validSets.indexOf(name); })
@@ -16,7 +16,7 @@ self.addEventListener('install', function (event) {
 });
 
 workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
-  cacheName: 'is-sw-58a1db',
+  cacheName: 'is-sw-954823',
 }));
 
 workbox.routing.registerRoute(new RegExp('/.*(:?/[^\\.]*/?)$'), function(context) {
