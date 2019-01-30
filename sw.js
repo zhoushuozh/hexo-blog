cda49462266846affc5157b328ff9b38c7fc9d3a
zhoushuo <zhoushuozh@gmail.com>
2019-01-30 16:02:15 +0800

    Site updated: 2019-01-30 16:02:13

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.keys().then(function (names) {
-      var validSets = ["is-sw-d54fc0","is-html-d54fc0"];
+      var validSets = ["is-sw-3d4965","is-html-3d4965"];
       return Promise.all(
         names
           .filter(function (name) { return !~validSets.indexOf(name); })
@@ -16,7 +16,7 @@ self.addEventListener('install', function (event) {
 });
 
 workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
-  cacheName: 'is-sw-d54fc0',
+  cacheName: 'is-sw-3d4965',
 }));
 
 workbox.routing.registerRoute(new RegExp('/.*(:?/[^\\.]*/?)$'), function(context) {
